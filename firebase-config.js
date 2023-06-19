d8330c340fcd0c4537e5382def68c9306470a746
2loopVoyvoda <94403004+2loopVoyvoda@users.noreply.github.com>
2023-06-19 20:19:30 +0300

    Trying to make the code work.

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDFXUT28pG0KiXcHrXHpnFTNfikeml1oZ8",
   authDomain: "sys112.firebaseapp.com",
-  databaseURL: "https://sys112-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://sys112-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "sys112",
   storageBucket: "sys112.appspot.com",
-  messagingSenderId: "<your-messaging-sender-id>",
+  messagingSenderId: "386925628090",
   appId: "1:386925628090:web:cbbd384b46d55ad6b95083",
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
